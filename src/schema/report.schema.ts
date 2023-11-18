43749248e4828173819bd685b43f11d973a1cdf6
Ahmed Hamada <ahmedhamada496@gmail.com>
2023-11-18 23:12:03 +0200

    channge the type of sessionid to number in validation

--- src/schema/report.schema.ts
+++ src/schema/report.schema.ts
@@ -1,12 +1,19 @@
 import { z } from "zod"
 
-const sessionId = z.string()
+const sessionId = z.number()
 const arabic = z.optional(z.string())
 const islamic = z.optional(z.string())
 const quran = z.optional(z.string())
 const comment = z.optional(z.string())
 const teacherId = z.string()
-const grade = z.string()
+const grade = z.enum([
+  "excellent",
+  "good",
+  "very good",
+  "average",
+  "below average",
+])
+
 const title = z.string()
 
 export const createReportSchema = z.object({
