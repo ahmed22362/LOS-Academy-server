53a066fa5a4c782ca8e2273400b9811db86a99db
ahmed hamada <ahmedhamada496@gmail.com>
2024-01-24 21:17:01 +0200

    remove log in validate

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -4,7 +4,6 @@ import { AnyZodObject } from "zod";
 const validate =
   (schema: AnyZodObject) =>
   (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.body);
     try {
       schema.parse({
         body: req.body,
